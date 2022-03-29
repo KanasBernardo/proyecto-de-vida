9827f75817d2a7aba9216fd7e3ee7ca7d360dac5
KanasBernardo <98572675+KanasBernardo@users.noreply.github.com>
2022-03-28 19:19:32 -0600

    efectos

---
+++ src/js/index.js
@@ -0,0 +1,85 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.navegacion', {
+    duration: 3000,
+    origin: 'botton',
+    distance: '-100px',
+});
+
+sr.reveal('.cuadro', {
+    duration: 3000,
+    origin: 'botton',
+    distance: '-100px',
+});
+
+sr.reveal('.info', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-400px',
+});
+
+sr.reveal('.infoV', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-400px',
+});
+
+sr.reveal('.table', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-200px',
+});
+
+sr.reveal('.infoV', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-400px',
+});
+
+//autobiografia
+sr.reveal('.infoAU', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-30px',
+});
+sr.reveal('.contImg', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-150px',
+});
+
+sr.reveal('.containerTi', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-400px',
+});
+
+sr.reveal('.contImgL', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-400px',
+});
+
+sr.reveal('.infoL', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-400px',
+});
+////////
+sr.reveal('.contImgN', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-400px',
+});
+
+sr.reveal('.infoNA', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-400px',
+});
+
+sr.reveal('.infoSU', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-400px',
+});
